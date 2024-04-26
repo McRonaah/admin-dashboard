9b95560b323025c0b3f600bea8561df4b8330ebd
McRonaah <ronaldcheruiyot342@gmail.com>
2024-04-26 15:24:05 +0300

    :art: updated sidebar pages :fire:

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -31,10 +31,10 @@ function App() {
             <main className="content">
               <Topbar setIsSidebar={setIsSidebar} />
               <Routes>
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
-                {/* <Route path="/invoices" element={<Invoices />} /> */}
-                {/* <Route path="/contacts" element={<Contacts />} /> */}
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/contacts" element={<Contacts />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/form" element={<Form />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
